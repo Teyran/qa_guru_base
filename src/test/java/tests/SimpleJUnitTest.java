d1cae4a7d57eb67bf8f26a52f57606b8095b3d02
Teiran Atamov <teiran_atamov@epam.com>
2023-10-27 00:27:21 +0500

    static imports applied

--- src/test/java/tests/SimpleJUnitTest.java
+++ src/test/java/tests/SimpleJUnitTest.java
@@ -1,6 +1,11 @@
 package tests;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class SimpleJUnitTest {
 
