cd9fd5d07a115e6d81d10309b29655de1779d114
Teiran Atamov <teiran_atamov@epam.com>
2023-11-07 18:13:56 +0500

    added home assignment for 4th lecture: Selenide #1 (fixed)

--- src/test/java/github/SelenideOnGithubTests.java
+++ src/test/java/github/SelenideOnGithubTests.java
@@ -19,55 +19,59 @@ public class SelenideOnGithubTests {
         pageLoadStrategy = "eager";
         holdBrowserOpen = false;
     }
+
     @AfterEach
     void afterEach() {
         closeWebDriver();
     }
 
     @Test
-    @DisplayName("Test for the first contributor Andrei Solntsev")
     void testAndreiSolntsevIsTheFirstContributor() {
         open("/selenide/selenide");
         $("div.Layout-sidebar").$(byText("Contributors"))
                 //.closest("BorderGrid-cell").$$("ul li").first().hover();
                 .closest("h2").sibling(0).$$("li").first().hover();
-
         $(".Popover-message").shouldHave(text("Andrei Solntsev"));
     }
 
     @Test
-    @DisplayName("Test for the first contributor Andrei Solntsev")
     void testJUnitCodeExamplePresentedOnSelenideWikiPage() {
+        String jUnit5Extend =
+                """ 
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """;
+
+        String jUnit5ExtendInside =
+                """
+                class Tests {
+                  @RegisterExtension
+                  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();
+                                
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """;
         open("/selenide/selenide");
         $("#wiki-tab").click();
         $$(".markdown-body a").findBy(text("Soft Assertions")).click();
         $$("h4 a").findBy(partialText("using JUnit5")).shouldBe(visible);
-        $$("pre").findBy(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-                                                                        "class Tests {\n" +
-                                                                        "  @Test\n" +
-                                                                        "  void test() {\n" +
-                                                                        "    Configuration.assertionMode = SOFT;\n" +
-                                                                        "    open(\"page.html\");\n" +
-                                                                        "\n" +
-                                                                        "    $(\"#first\").should(visible).click();\n" +
-                                                                        "    $(\"#second\").should(visible).click();\n" +
-                                                                        "  }\n" +
-                                                                        "}"))
-                .shouldBe(visible);
-
-        $$("pre").findBy(text("class Tests {\n" +
-                                            "  @RegisterExtension \n" +
-                                            "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
-                                            "\n" +
-                                            "  @Test\n" +
-                                            "  void test() {\n" +
-                                            "    Configuration.assertionMode = SOFT;\n" +
-                                            "    open(\"page.html\");\n" +
-                                            "\n" +
-                                            "    $(\"#first\").should(visible).click();\n" +
-                                            "    $(\"#second\").should(visible).click();\n" +
-                                            "  }\n" +
-                                            "}"))
-                .shouldBe(visible);
+        $$("pre").findBy(text(jUnit5Extend)).shouldBe(visible);
+        $$("pre").findBy(text(jUnit5ExtendInside)).shouldBe(visible);
     }
 }
