113f85341a010420e46cb0dca736122fcb9dc12e
Teiran Atamov <teiran_atamov@epam.com>
2024-01-05 17:25:18 +0500

    added simple tests for jenkins job configuration

---
+++ src/test/java/tests/simple/predefined/PositiveTests.java
@@ -0,0 +1,35 @@
+package tests.simple.predefined;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class PositiveTests {
+
+    @Test
+    void someTest() {
+        assertTrue(true);
+    }
+    @Test
+    void someTest1() {
+        assertTrue(true);
+    }
+    @Test
+    void someTest2() {
+        assertTrue(true);
+    }
+    @Test
+    void someTest3() {
+        assertTrue(true);
+    }
+    @Test
+    void someTest4() {
+        assertTrue(true);
+    }
+    @Test
+    void someTest5() {
+        assertTrue(true);
+    }
+}
